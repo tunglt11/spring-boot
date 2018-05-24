ed07d672a027dac886896153c288c02778bb2ece
tunglt11 <pakbafi@gmail.com>
2018-05-24 14:43:06 +0700

    commit

---
+++ HelloAppEngine/src/main/java/com/example/helloappengine/HelloAppEngineApplication.java
@@ -0,0 +1,12 @@
+package com.example.helloappengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloAppEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloAppEngineApplication.class, args);
+	}
+}
---
+++ HelloAppEngine/src/test/java/com/example/helloappengine/HelloAppEngineApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.helloappengine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloAppEngineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
