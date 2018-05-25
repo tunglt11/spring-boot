90a5cf4ea70503da4699642e75293bd3d7080afc
Tuepm <39039522+Tuepm@users.noreply.github.com>
2018-05-25 11:51:35 +0700

    commit source

---
+++ src/main/java/com/example/helloappengine/bean/Idol.java
@@ -0,0 +1,31 @@
+package com.example.helloappengine.bean;
+
+public class Idol {
+
+	private String id;
+
+	private String name;
+	
+
+	public Idol(String id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+}
